6864fcc324cd3aaebfdbfaed11eed09fd6e3cf77
chankei1 <kkeisukee222@gmail.com>
2015-05-01 00:24:50 +0900

    Initial Commit

---
+++ CSVAttachedToMailer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSVAttachedToMailer
+//
+//  Created by Keisuke.K on 2015/05/01.
+//  Copyright (c) 2015年 Keisuke.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
